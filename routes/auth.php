0e9405fd915dea4c2676538346517ef5265458a4
devslav2 <developers@domanilavoro.com>
2025-10-05 14:52:59 +0200

    fix logout

--- routes/auth.php
+++ routes/auth.php
@@ -33,7 +33,7 @@ Route::middleware('guest')->group(function () {
       ->name('password.update');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
       ->name('verification.notice');
 
